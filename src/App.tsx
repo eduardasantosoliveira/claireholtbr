42a2a9feb63db3ca9d0c49654e864ad5025148f9
Eduarda Oliveira <duda.holt@gmail.com>
2025-04-25 12:14:57 +0100

    set up and base page

---
+++ src/App.tsx
@@ -0,0 +1,31 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="background"></div>
+      <div className="content">
+        <h1>CLAIREHOLT BRASIL</h1>
+        <p>
+          O Claire Holt Brasil foi o maior fã site brasileiro dedicado a atriz
+          australiana Claire Holt. O CHBR esteve no ar de 2013 a 2022. O site
+          infelizmente foi encerrado devido a falta de disponibilidade da equipe
+          para mantê-lo em funcionamento, mas essa página permanecerá online
+          como forma de agradecimento e demonstração da nossa alegria em ter
+          feito parte de algo tão grande e especial, e também em homenagem ao
+          nosso carinho e admiração pela Claire, que continua forte até os dias
+          de hoje.
+        </p>
+        <p>
+          Caso queira entrar em contato sobre qualquer assunto, utilize o e-mail
+          a seguir:
+        </p>
+        <div className="email">
+          <p>eduarda.so@outlook.com</p>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
